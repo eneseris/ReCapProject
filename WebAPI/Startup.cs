59d6ade79fae60a5c699fae7af59a3f444dfcc26
Enes Eriş <eneseris123@hotmail.com>
2021-02-20 00:28:46 +0300

    Autofac

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -30,11 +30,11 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<ICarService, CarManager>();
-            services.AddSingleton<ICarDal, EfCarDal>();
+            //services.AddSingleton<ICarService, CarManager>();
+            //services.AddSingleton<ICarDal, EfCarDal>();
 
-            services.AddSingleton<IColorService, ColorManager>();
-            services.AddSingleton<IColorDal, EfColorDal>();
+            //services.AddSingleton<IColorService, ColorManager>();
+            //services.AddSingleton<IColorDal, EfColorDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
