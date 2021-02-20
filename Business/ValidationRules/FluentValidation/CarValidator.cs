18be30cd258b0ab3677e4ed25071015adf8d038b
Enes Eriş <eneseris123@hotmail.com>
2021-02-20 15:53:05 +0300

    FluentValidations

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.ModelYear).LessThan(2022);
+            RuleFor(c => c.Description).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(100000).When(c => c.ModelYear >= 2020);
+        }
+    }
+}
