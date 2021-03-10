731ef4b113578f4f241e653f081536c0d01a0825
Enes Eriş <eneseris123@hotmail.com>
2021-03-10 13:10:42 +0300

    Cache

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -56,6 +56,7 @@ namespace WebAPI
                         IssuerSigningKey=SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                 };
             });
+            services.AddDependencyResolvers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
